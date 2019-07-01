7efabd9783485b7f5f18eaa30e55cea6c14a668f
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-01 01:20:19 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,50 @@
 # Your Code Here
 def map(source_array)
-  
+  i = 0 
+  array = [] 
+  while i < source_array.length do 
+    array << yield(source_array[i])
+    i += 1
+  end
+  array
 end
 
 def reduce(source_array, sp = nil)
-  
+  if sp 
+    accum = sp 
+    i = 0
+  else 
+    accum = source_array[0]
+    i = 1 
+  end 
+  while i < source_array.length
+    accum = yield(accum, source_array[i])
+    i += 1 
+  end
+  accum
 end 
 
+
+
+
+#  i = 0
+#  while i < source_array.length do
+#    return false if !source_array[i]
+#    i += 1
+#  end
+#  yield(source_array, true)
+
+#  new = 0
+#  i = 0
+#  while i < source_array.length do
+#    new += source_array[i]
+#    i += 1
+#  end
+#  yield(new, starting_value)
+
+#  i = 0
+#  while i < source_array.length do
+#    return true if source_array[i]
+#    i += 1
+#  end
+#  yield(source_array, false)
