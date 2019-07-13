f83adedbd2c5ec14bf9f760116a4ea1c694f4bef
Belle P <bellepx3@gmail.com>
2019-07-13 19:45:36 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+def my_own_map(source_array)  
+  new = []
+  source_array.map{|element| element * -1}
+end
+
+def map_to_no_change(source_array)
+    source_array.map{|element| element}
+end 
+
+def map_to_double(source_array)
+  source_array.map {|element| element * 2}
+end 
+
+def map_to_square(source_array) 
+  source_array.map {|element| element ** 2}
+end 
+
+def reduce_to_total(source_array, starting_point = 0)
+  source_array.reduce(starting_point){|total, number| total + number}
+end 
+
+def reduce_to_all_true(source_array)
+  source_array.all? {|element| !!element}
+end 
+
+def reduce_to_any_true(source_array)
+  source_array.any? {|element| !!element}
+end 
