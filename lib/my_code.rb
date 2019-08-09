1784b046510499d426124e8b26adc5c22939ebef
Jonathan Koss <jonathan_koss@yahoo.com>
2019-08-09 20:19:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,24 +23,3 @@ def reduce(source_array, starting_point = nil)
   accum
 end 
 
-#  i = 0
-#  while i < source_array.length do
-#    return false if !source_array[i]
-#    i += 1
-#  end
-#  yield(source_array, true)
-
-#  new = 0
-#  i = 0
-#  while i < source_array.length do
-#    new += source_array[i]
-#    i += 1
-#  end
-#  yield(new, starting_value)
-
-#  i = 0
-#  while i < source_array.length do
-#    return true if source_array[i]
-#    i += 1
-#  end
-#  yield(source_array, false)
