6fb0f02f9e5e742eba93e5143e6f53c2833ba5cd
Aibek Ozhorov <ozhorov@gmail.com>
2019-08-15 21:00:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,39 @@
-# Your Code Here
+
+def map(source_array)
+  basket = []
+  i = 0
+   while i < source_array.length do
+   basket.push(yield(source_array[i]))
+    i+=1
+ end
+ basket
+end
+
+def reduce(array,sp=nil)
+
+  if sp 
+    i=0
+    sum_total = sp
+  else 
+    sum_total = array[0]
+    i = 1
+  end
+  while i<array.length do
+    sum_total = yield(sum_total,array[i])
+  i+=1
+  end
+  sum_total
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
