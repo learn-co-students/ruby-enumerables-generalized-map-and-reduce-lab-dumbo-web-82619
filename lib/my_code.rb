916fefed68607ba5f36a85bef29bed733e5446e4
Franky Liu <cuifranky95@gmail.com>
2019-08-21 18:37:32 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map(array)
+  newarray = []
+  array.each do |index|
+    newarray.push(yield(index))
+  end
+  return newarray
+end
+
+def reduce(array, starting_point = 0)
+ newvalue = starting_point
+  array.each do |index|
+    newvalue = yield(index, newvalue)
+  end
+   if newvalue.is_a? Integer
+    return newvalue
+  else 
+    return !!newvalue
+  end
+end
