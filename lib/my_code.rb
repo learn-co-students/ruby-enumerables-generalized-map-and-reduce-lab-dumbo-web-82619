16abece99ee6139e8e70ecc5782ca6a320831fd3
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-24 23:30:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+def map(arr)
+  new = []
+  for i in arr do
+    new.push(yield(i)) 
+  end
+  
+  return new
+end
+
+def reduce(arr, acc = 0)
+  currentValue = acc
+  for i in arr do
+      if i == nil 
+        p i
+        currentValue = false  
+        p currentValue
+      end
+      currentValue = yield(currentValue, i)
+  end
+  
+  return currentValue
+end
